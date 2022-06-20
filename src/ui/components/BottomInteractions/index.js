7b4c8647fb5fa9e65e9320e62167982193eb1237
Pelayo Suárez <pesuafer@gmail.com>
2022-06-20 16:04:15 +0200

    feat: added styles to the component

--- src/ui/components/BottomInteractions/index.js
+++ src/ui/components/BottomInteractions/index.js
@@ -8,16 +8,12 @@ const BottomInteractions = ({ buttons }) => {
   return (
     <div className="bottom-interactions">
        {buttons.map((button, index) => (
-            <Button key={index.toString} text={button.text} type={button.type} isDisabled={button.isDisabled} />
+            <Button key={index.toString()} text={button.text} type={button.type} isDisabled={button.isDisabled} />
         ))}
     </div>
   );
 };
 
-BottomInteractions.defaultProps = {
-    type: 'default', 
-    isDisabled: false,
-}
 
 BottomInteractions.prototypes = {
   buttons: PropTypes.array.isRequired,
